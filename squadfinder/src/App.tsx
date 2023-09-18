c9635614c1372717f6864dfe095f7796261ccb16
gvs006 <40977112+gvs006@users.noreply.github.com>
2023-09-18 10:27:20 -0300

    react bootstrap install package

--- squadfinder/src/App.tsx
+++ squadfinder/src/App.tsx
@@ -3,22 +3,25 @@ import logo from './assets/login/logo-full-white.png';
 import './App.css';
 import { LoginButton } from './components/Button/Button.style';
 import Header from './components/Header/Header';
+import { Container } from 'react-bootstrap';
+
 
 function App() {
   return (
     <>
     <Header></Header>
-    <div className="App">
-      <header className="App-header">
+    
+    <Container>
+
+   
         <img src={logo} alt="logo" />
         <p>
         MANUTENÇÃO
         <br></br>
          <LoginButton>Entrar</LoginButton>
         </p>
-      
-      </header>
-    </div>
+
+    </Container>
     </>
   );
 }
