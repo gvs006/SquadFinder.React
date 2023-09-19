69a7a17a8659917616cfb1a02b5ee6da9e05046d
gvs006 <40977112+gvs006@users.noreply.github.com>
2023-09-19 01:18:24 -0300

    testing

--- squadfinder/src/App.tsx
+++ squadfinder/src/App.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import logo from './assets/login/logo-full-white.png';
-import './App.css';
-import { LoginButton } from './components/Button/Button.style';
-import Header from './components/Header/Header';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import logo from "./assets/login/logo-full-white.png";
+import "./App.css";
+import { LoginButton } from "./components/Button/Button.style";
+import { Col, Container, Row } from "react-bootstrap";
+import styled from "styled-components";
+import Header from "./components/Header/Header";
 
+export const ContainerStyle = styled.div`
+  text-align: center;
+  height: 100vh;
+  background-color: #fff;
+
+`;
 
 function App() {
   return (
     <>
-    <Header></Header>
-    
-    <Container>
-
-   
-        <img src={logo} alt="logo" />
-        <p>
-        MANUTENÇÃO
-        <br></br>
-         <LoginButton>Entrar</LoginButton>
-        </p>
-
-    </Container>
+      <ContainerStyle>
+        <Header></Header>
+        <Container fluid>
+          <Row>
+            <Col>
+              <LoginButton>Entrar</LoginButton>
+            </Col>
+          </Row>
+        </Container>
+      </ContainerStyle>
     </>
   );
 }
--- squadfinder/src/components/Header/Header.tsx
+++ squadfinder/src/components/Header/Header.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from "../../assets/login/logo-full-white.png";
+import img from "../../assets/login/hero-bg-pqn.jpg"
+import styled from "styled-components";
 
+export const HeaderStyle = styled.div`
+  background-image: linear-gradient(180deg, #00000011 0%, #ffffff 88.02%), url(${img});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100% auto;
+  height: 50vh;
+`;
 function Header() {
   return (
-    <>Header</>
+    <>
+      <HeaderStyle>
+        <img src={logo} alt="logo" />
+      </HeaderStyle>
+    </>
   );
 }
 
